ef37f9f22445a7c7c0d422dbb3206fda49f15380
zyrouge <naveencnaveen96@gmail.com>
2022-03-20 22:19:29 +0530

    refactor: fix date (hopefully #3)

--- backend/src/core/articles/render.ts
+++ backend/src/core/articles/render.ts
@@ -33,7 +33,9 @@ export const RenderArticle = async (
         title: pMeta.title.trim(),
         description: md.renderInline(pMeta.description).trim(),
         tags: pMeta.tags.split(",").map((x: string) => x.trim()),
-        time: pTime.toISOString(),
+        time: pTime.toLocaleString("en-US", {
+            timeZone: "GMT",
+        }),
     };
 
     const content = md.render(rContent).trim();
