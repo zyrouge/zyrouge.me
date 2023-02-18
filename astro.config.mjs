3ecc07891b539f9be9ad7ccc0e014c724c2d2cd1
Zyrouge <zyrouge@hotmail.com>
2023-02-18 18:42:40 +0530

    refactor: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@ import path from "node:path";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({ config: { applyBaseStyles: false } }), mdx()],
+    site: "https://zyrouge.is-a.dev",
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+        mdx(),
+        sitemap(),
+    ],
     output: "static",
     vite: {
         resolve: {
